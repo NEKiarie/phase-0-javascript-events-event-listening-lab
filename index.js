cdbbda78f6b22606d6e280d2b6eeea3b1e104ee6
Elvis <s49188705@gmail.com>
2022-08-10 12:07:35 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+  const buttonclick = document.getElementById("button");
+
+  function clickAlert() {
+    alert("Button has been clicked!");
+  }
+
+  buttonclick.addEventListener("click", clickAlert);
 }
